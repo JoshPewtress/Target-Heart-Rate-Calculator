3075382531bdf1646361308bf90772c86e03c721
Josh Pewtress <Joshpewtress@gmail.com>
2023-04-05 19:29:28 -0600

    FInished the HeartRates class

---
+++ HeartRates.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Target_Heart_Rate_Calculator
+{
+   internal class HeartRates
+   {
+      private string _firstName;
+      private string _lastName;
+      private int _birthYear;
+      private int _currentYear;
+
+      public HeartRates(string firstName, string lastName, int birthYear, int currentYear)
+      {
+         _firstName = firstName;
+         _lastName = lastName;
+         _birthYear = birthYear;
+         _currentYear = currentYear;
+      }
+
+      public string _FirstName
+      {
+         get { return _firstName; }
+         set { _firstName = value; }
+      }
+      public string _LastName
+      {
+         get { return _lastName; }
+         set { _lastName = value; }
+      }
+      public int _BirthYear
+      {
+         get { return _birthYear; }
+         set { _birthYear = value; }
+      }
+      public int _CurrentYear
+      {
+         get { return _currentYear; }
+         set { _currentYear = value;  }
+      }
+      // Property to calculate and display person's age in years
+      public int Age
+      {
+         get { return _currentYear - _birthYear; }
+      }
+
+      // Property to calculate and display person's maximum heart rate
+      public int MaxHeartRate
+      {
+         get { return 220 - Age; }
+      }
+
+      // Propertys to calculate and display minimum and maximum target heart rates
+      public int MinTargetHeartRate
+      {
+         get { return (int) (.50 * MaxHeartRate); }
+      }
+
+      public int MaxTargetHeartRate
+      {
+         get { return (int)(.85 * MaxHeartRate);  }
+      }
+
+   }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@
    {
       static void Main(string[] args)
       {
-         
+         string firstName;
+         string lastName;
+         int birthYear;
+         int currentYear;
+
+         Console.Write("Enter your First Name: ");
+         firstName = Console.ReadLine();
+
+         Console.Write("Enter your Last Name: ");
+         lastName = Console.ReadLine();
+
+         Console.Write("Enter your birth year: ");
+         birthYear = int.Parse(Console.ReadLine());
+
+         Console.Write("Enter the current year: ");
+         currentYear = int.Parse(Console.ReadLine());
+
+         HeartRates Person1 = new HeartRates(firstName, lastName, birthYear, currentYear);
+
+         Console.WriteLine($"\nName: {Person1._FirstName} {Person1._LastName}\n" +
+            $"Birth Year: {Person1._BirthYear}\n" +
+            $"Age: {Person1.Age} years old.\n\n" +
+            $"Heart Rates\n\n" +
+            $"Maximum Heart Rate: {Person1.MaxHeartRate}\n" +
+            $"Target Heart Rate is between {Person1.MinTargetHeartRate} and {Person1.MaxTargetHeartRate}.");
       }
    }
 }
